627b83a2f36db27bada66a64e6fe0f5c33df3acc
dpb15 <brannerchinese@gmail.com>
2014-05-27 16:43:08 -0400

    add README and remove debug-prints

---
+++ README.md
@@ -0,0 +1,21 @@
+## URL Shortener
+
+This project uses Chinese characters ("kanji", as a shorthand name) as the basis of shortened URLs. 
+
+### To Install
+
+ 1. This is a Python3 project. The only dependency outside of the `requirements.txt` file is `sqlite3`.
+
+ 1. Create database:
+
+        sqlite3 shortener.db < code/create_shortener_database.sql
+
+### Ideas
+
+ 1. There are some 27K kanji in Unicode Plane 0, so far fewer of them are needed than ASCII to generate the same number of strings of a given length: two-kanji strings number over 76M and three-kanji strings over 21T. We assume here that the purpose of shortened URLs is chiefly for human convenience, so it doesn't matter that each kanji is represented internally by a four-place hexadecimal sequence.
+
+ 1. Shortened paths are assigned randomly rather than by a recoverable hashing function. We are not concerned with duplicate URLs in the database, either, sinceshortened strings are plentiful.
+
+ 1. Note that because the shortened strings are generated randomly, they are unlikely to be meaningful.
+
+[end]
--- code/shorten.py
+++ code/shorten.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 # shorten.py
 # David Prager Branner
-# 20140527
+# 20140527, works.
 
 import sqlite3
 import random
 
-"""Use kanji as basis of shortened URLs."""
+"""Using kanji, generate a string for use as the path in a shortened URL."""
 
 # Generate a randomly shuffled list of all kanji in Unicode Plane 0.
 # Valid codepoints range from hex 3400:9FFF i.e. decimal 13312:40959, inclusive.
@@ -14,14 +14,11 @@ codepoints = list(range(13312, 40960))
 
 
 def shorten(url, db='shortener.db'):
-    """Return a unique string to represent input string s."""
-    # Idea: since the purpose of shortening is for human convenience, use
-    # random strings of Chinese characters rather than ASCII.
+    """Return a unique string to represent input string url."""
     connection = sqlite3.connect('../' + db)
     with connection:
         cursor = connection.cursor()
         path = get_path(cursor, url)
-        # If kanji already in use, must generate new string of >1 kanji.
     return path
 
 def get_path(cursor, url):
@@ -34,7 +31,6 @@ def get_path(cursor, url):
         for i in range(20):
             prospective_shortened = ''.join(
                     [chr(random.choice(codepoints)) for i in range(n)])
-            print('{}: {}'.format(n, prospective_shortened), end=' ')
             # Try to insert; IntegrityError means already exists.
             try:
                 cursor.execute(
@@ -46,7 +42,6 @@ def get_path(cursor, url):
                 prospective_shortened = None
                 continue
         n += 1
-    print('successful')
     return prospective_shortened
 
 # To do: add a table to keep track of how many 1-char short forms there are ,
